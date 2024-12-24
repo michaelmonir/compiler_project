148df158f7f9fd478f20120b957329c5d5b760ac
mmonir <mmonir@webalo.net>
2024-12-24 11:27:33 +0200

    finished the print function, now will begin testing

--- src/Parser/ParcerStream.cpp
+++ src/Parser/ParcerStream.cpp
@@ -4,20 +4,50 @@
 
 #include "ParcerStream.h"
 #include <stack>
-#include "ParseStructs.h"
+
+class Node {
+public:
+    ParseUnit unit;
+    vector<Node*> children;
+
+    Node(ParseUnit unit) : unit(unit) {}
+};
 
 map<ParseUnit, Token> terminal_map;
-map<Token, ParseUnit> inverse_terminal_map;
 map<ParseUnit,map<Token, ParseTableItem>> parse_table;
 
-stack<ParseUnit> parse_stack;
+stack<Node*> parse_stack;
+Node *root;
+ParseUnit start_symbol;
+
+void initialize_stack() {
+    root = new Node(start_symbol);
+    parse_stack.push(root);
+}
+
+void left_most_print_node(Node *node) {
+    if (!node) return;
+
+    if (node->unit.type == ParseUnitType::TERMINAL) {
+        cout << terminal_map[node->unit].token_name << " ";
+    } else {
+        for (Node* child : node->children) {
+            left_most_print_node(child);
+        }
+    }
+}
+
+void left_most_print_root() {
+    left_most_print_node(root);
+}
 
 void stream_process_input_token(Token token) {
     if (parse_stack.empty()) {
         cout << "EMPTY STACK: Error at token " << token.token_name << endl;
         return;
     }
-    ParseUnit top = parse_stack.top();
+    Node* topNode = parse_stack.top();
+    ParseUnit top = topNode->unit;
 
     if (top.type == ParseUnitType::TERMINAL) {
         if (terminal_map[top] != token) {
@@ -36,8 +66,11 @@ void stream_process_input_token(Token token) {
             cout << "Error at token " << token.token_name << endl;
             stream_process_input_token(token);
         } else {
+            parse_stack.pop();
             for (auto it = item.rhs.rbegin(); it != item.rhs.rend(); it++) {
-                parse_stack.push(*it);
+                Node *newNode = new Node(*it);
+                topNode->children.push_back(newNode);
+                parse_stack.push(newNode);
             }
         }
     }
--- src/Parser/ParcerStream.h
+++ src/Parser/ParcerStream.h
@@ -5,7 +5,10 @@
 #ifndef PARCERSTREAM_H
 #define PARCERSTREAM_H
 
+#include "ParseStructs.h"
 
-
+void initialize_stack();
+void left_most_print_root();
+void stream_process_input_token(Token token);
 
 #endif //PARCERSTREAM_H
